4ed0769ca3cacd875ce9dde054e031b7418b35d3
Papageno <3258359379@qq.com>
2025-04-22 18:52:48 +0800

    Mission boxmesh file

--- XEngine/Engine/Mesh/BoxMesh.cpp
+++ XEngine/Engine/Mesh/BoxMesh.cpp
@@ -27,12 +27,53 @@ BoxMesh* BoxMesh::CreateMesh()
 	MeshRenderData.PosData.push_back(FVector{ XMFLOAT3{1.f,1.f,1.f}, XMFLOAT4{Colors::White} });
 	MeshRenderData.PosData.push_back(FVector{ XMFLOAT3{1.f,0.f,1.f}, XMFLOAT4{Colors::White} });
 	
+	//front
 	MeshRenderData.IndexData.push_back(0);
 	MeshRenderData.IndexData.push_back(1);
 	MeshRenderData.IndexData.push_back(2);
 	MeshRenderData.IndexData.push_back(0);
 	MeshRenderData.IndexData.push_back(2);
 	MeshRenderData.IndexData.push_back(3);
+
+	//back
+	MeshRenderData.IndexData.push_back(4);
+	MeshRenderData.IndexData.push_back(6);
+	MeshRenderData.IndexData.push_back(5);
+	MeshRenderData.IndexData.push_back(4);
+	MeshRenderData.IndexData.push_back(6);
+	MeshRenderData.IndexData.push_back(7);
+
+	//left
+	MeshRenderData.IndexData.push_back(4);
+	MeshRenderData.IndexData.push_back(5);
+	MeshRenderData.IndexData.push_back(1);
+	MeshRenderData.IndexData.push_back(4);
+	MeshRenderData.IndexData.push_back(1);
+	MeshRenderData.IndexData.push_back(0);
+
+	//right
+	MeshRenderData.IndexData.push_back(3);
+	MeshRenderData.IndexData.push_back(2);
+	MeshRenderData.IndexData.push_back(6);
+	MeshRenderData.IndexData.push_back(3);
+	MeshRenderData.IndexData.push_back(6);
+	MeshRenderData.IndexData.push_back(0);
+
+	//up
+	MeshRenderData.IndexData.push_back(1);
+	MeshRenderData.IndexData.push_back(5);
+	MeshRenderData.IndexData.push_back(6);
+	MeshRenderData.IndexData.push_back(1);
+	MeshRenderData.IndexData.push_back(6);
+	MeshRenderData.IndexData.push_back(0);
+
+	//down
+	MeshRenderData.IndexData.push_back(4);
+	MeshRenderData.IndexData.push_back(0);
+	MeshRenderData.IndexData.push_back(6);
+	MeshRenderData.IndexData.push_back(4);
+	MeshRenderData.IndexData.push_back(3);
+	MeshRenderData.IndexData.push_back(0);
 
 	BoxMesh* Result = new BoxMesh();
 	Result->BuildMesh(&MeshRenderData);
